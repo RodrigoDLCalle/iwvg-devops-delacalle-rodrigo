57f8ffcced016cce2c59a483fc2876b9e3c6fd9a
Rodrigo de la Calle <rodricalle@usal.es>
2023-09-27 18:54:11 +0200

    added testFindUserFamilyNameByAllNegativeSignFractionDistinct issue#24

--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,17 +21,17 @@ class SearchesTest {
         List<Fraction> fractions1 = new ArrayList<>();
         fractions1.add(new Fraction(1, 2));
         fractions1.add(new Fraction(3, 4));
-        User user1 = new User("1", "John", "Doe", fractions1);
+        User user1 = new User("1", "Rodrigo", "Calle", fractions1);
 
         List<Fraction> fractions2 = new ArrayList<>();
         fractions2.add(new Fraction(2, 5));
         fractions2.add(new Fraction(7, 6));
-        User user2 = new User("2", "Jane", "Smith", fractions2);
+        User user2 = new User("2", "Lucia", "Gonzalez", fractions2);
 
         List<Fraction> fractions3 = new ArrayList<>();
         fractions3.add(new Fraction(5, 3));
         fractions3.add(new Fraction(10, 5));
-        User user3 = new User("3", "Alice", "Johnson", fractions3);
+        User user3 = new User("3", "Paula", "Alonso", fractions3);
 
         users.add(user1);
         users.add(user2);
@@ -56,10 +55,20 @@ class SearchesTest {
     @Test
      void testFindDecimalImproperFractionByUserName() {
 
-        Stream<Double> result = searches.findDecimalImproperFractionByUserName("Alice");
+        Stream<Double> result = searches.findDecimalImproperFractionByUserName("Paula");
         List<Double> resultList = result.toList();
 
         assertEquals(2, resultList.size());
         assertEquals(1.66, resultList.get(0), 0.01);
+    }
+
+    @Test
+    void testFindUserFamilyNameByAllNegativeSignFractionDistinct() {
+        Stream<String> result = searches.findUserFamilyNameByAllNegativeSignFractionDistinct();
+
+        List<String> resultList = result.toList();
+
+        // Aserciones
+        assertEquals(0, resultList.size());
     }
 }
